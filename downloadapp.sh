05cb12b9839f3ae292ee77d578b489adcc6676a7
keerthi <ssk.ram@gmail.com>
2023-12-07 19:47:08 +0530

    minor

--- downloadapp.sh
+++ downloadapp.sh
@@ -1,4 +1,7 @@
 # instructions here
 # https://github.com/Project-MONAI/tutorials/blob/main/monailabel/monailabel_pathology_nuclei_segmentation_QuPath.ipynb
-monailabel apps --download --name pathology --output /workspacedir/monailabel_appdir 
+
+# monailabel apps --download --name pathology --output /workspacedir/monailabel_appdir 
+
+# monailabel apps --download --name monaibundle --output /workspacedir/monailabel_appdir 
 
--- startserver.sh
+++ startserver.sh
@@ -1,3 +1,13 @@
 # instructions here
 # https://github.com/Project-MONAI/tutorials/blob/main/monailabel/monailabel_pathology_nuclei_segmentation_QuPath.ipynb
-monailabel start_server --app /workspacedir/monailabel_appdir/pathology --studies /data/special/web_viewer_verify/tiles/
+
+# monailabel start_server --app /workspacedir/monailabel_appdir/pathology --studies /data/special/web_viewer_verify/tiles/
+
+monailabel start_server --app /workspacedir/monailabel_setup/custom_appdir/myapp --studies /data/special/web_viewer_verify/tiles/
+
+# https://github.com/Project-MONAI/MONAILabel/tree/0.7.0/sample-apps/monaibundle
+# monailabel start_server --app /workspacedir/monailabel_appdir/monaibundle --studies /data/special/web_viewer_verify/tiles/ --conf models pathology_nuclei_segmentation_classification_v0.2.1 \
+#   --conf epistemic_model pathology_nuclei_segmentation_classification_v0.2.1 \
+#   --conf epistemic_max_samples 0 \
+#   --conf epistemic_simulation_size 5 \
+#   --conf epistemic_dropout 0.2
